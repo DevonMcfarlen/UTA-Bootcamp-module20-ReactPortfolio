373a3574cc79dc58fe99ae822a7ed8da19da0eee
DevonMcfarlen <mcfarlendevon@gmail.com>
2023-12-04 17:05:09 -0600

    created navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar() {
+    return (
+        <ul>
+            <li>
+                <a href="#about-me">About Me</a>
+            </li>
+            <li>
+                <a href="#work">Work</a>
+            </li>
+            <li>
+                <a href="#contact-me">Contact Me</a>
+            </li>
+            <li>
+                <a href="#resume">Resume</a>
+            </li>
+        </ul>
+    )
+}
+
+export default Navbar;
